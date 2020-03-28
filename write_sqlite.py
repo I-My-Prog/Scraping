481eaa243df63e7812cee9988288a49d06b927e6
Akata_M <eitaroumo@gmail.com>
2020-03-29 01:54:39 +0900

    remove check code Add_SecCode => SQLMatch(write_sqlite.py)

--- write_sqlite.py
+++ write_sqlite.py
@@ -5,6 +5,9 @@ pattern = '\d{4}'
 repatter = re.compile(pattern)
 
 class CreateTable():
+'''
+テーブルを作成する
+'''
     def __init__(self):
         conn = sqlite3.connect('Data.sqlite3')
         c = conn.cursor()
@@ -16,21 +19,43 @@ class CreateTable():
         conn.close
 
 class SQLMatch():
+'''
+dbnameに指定されたDBにtarget(int)が存在するか調べる
+返り値はブール型
+'''
     def __init__(self,dbname,target):
-
-class Add_SecCode():
-    def __init__(self,code):
         conn = sqlite3.connect('Data.sqlite3')
         c = conn.cursor()
-        c.execute('select * from SecCodeUseage')
+        c.execute('select * from ?',(dbname,))
         for row in c:
-            if row[0] == code and repatter.match(str(row[0])):
-                c.execute("insert into SecCodeList values ?",(code,))
-#        if SCValid:
-                print("Success")
+            if row[0] == target:
+                ret_val = bool(1)
+            else:
+                ret_val = bool(0)
         print("Log Out from DB...")
         conn.commit
         conn.close
+        return ret_val
+
+class Add_SecCode():
+'''
+codeがSecCodeUseageに存在するかつ４桁の数字か確認[SQLMatch()]
+if ある：
+    codeがSecCodeListに存在するか確認[SQLMatch()]
+    if ある：
+        エラーを表示する
+    if ない：
+        SecCodeListにcodeを追加する
+if ない：
+    エラーを表示する
+返り値はなし
+'''
+    def __init__(self,code):
+#4桁の数字か？
+#if repatter.match(str(row[0])):
+#SecCodeListにcodeを追加する
+#   c.execute('insert into ? values ?',(dbname,target))
+        pass
 
 #CreateTable()
 
